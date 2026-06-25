95975e982aed92b3c7ad35d5ba0dec45d3217c33
NADEEM604539 <ndm604539@gmail.com>
2026-06-25 15:06:20 +0500

    pushing text splitters complete

--- text-splitters.py
+++ text-splitters.py
@@ -1,16 +1,27 @@
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter, Language 
 from langchain_community.document_loaders import PyPDFLoader , DirectoryLoader
+from langchain_experimental.text_splitter import SemanticChunker
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=20)
-# pdf_load = PyPDFLoader('Nadeem-Mushtaq_Resume.pdf')
-pdfs = DirectoryLoader(
-    "",
-    glob="**/*.pdf",
-    loader_cls=PyPDFLoader
-)
+# text_splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=20)
+pdfs = PyPDFLoader('Nadeem-Mushtaq_Resume.pdf')
+# pdfs = DirectoryLoader(
+#     "",
+#     glob="**/*.pdf",
+#     loader_cls=PyPDFLoader
+# )
 
 docs = pdfs.lazy_load()
 
-text = text_splitter.split_documents(docs)
+# text = text_splitter.split_documents(docs)
 
-print(text)
+text = RecursiveCharacterTextSplitter.from_language(
+    language= Language.PYTHON,
+    chunk_size=100,
+    chunk_overlap=10
+)
+
+docs = text.split_documents(docs)
+print(docs)
+
+
+
