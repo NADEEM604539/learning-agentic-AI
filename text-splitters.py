0401b661975151c296067d8ad510c2a248894ff9
NADEEM604539 <ndm604539@gmail.com>
2026-06-25 14:48:41 +0500

    pushing splitters

---
+++ text-splitters.py
@@ -0,0 +1,16 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader , DirectoryLoader
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=20)
+# pdf_load = PyPDFLoader('Nadeem-Mushtaq_Resume.pdf')
+pdfs = DirectoryLoader(
+    "",
+    glob="**/*.pdf",
+    loader_cls=PyPDFLoader
+)
+
+docs = pdfs.lazy_load()
+
+text = text_splitter.split_documents(docs)
+
+print(text)
